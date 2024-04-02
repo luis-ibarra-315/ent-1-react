906e94516d3f516bb5088761e03ef8c49abc578e
Luis ibarra <luisdoranceibarrasantamaria@gmail.com>
2024-04-01 21:51:16 -0500

    se arreglan las imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import getRandom from './services/getRandom';
 import {useState} from 'react';
 import ShowPhrase from './components/ShowPhrase';
 import ButtonPhrase from './components/buttonPhrase';
-
 import arrayImages from './utils/images.json';
 
 function App() {
@@ -14,8 +13,10 @@ function App() {
   
  const[image, setImage] = useState(getRandom(arrayImages));
 
-  const objStyles ={
-  backgroundImage:`url(../public/fortuna-2/fondo${image}.png)`,
+ const url = `../fortuna-2/fondo${image}.png`
+
+   const objStyles ={
+  backgroundImage:`url(${url})`
    }
 
   return (
